ceaf9ca9278fc0d8933f89343c02771b70caa6dc
DH <50+dh@users.noreply.1io.com>
2024-02-14 16:04:28 +0100

    feat(cores/pin): improved comments, remove cid key when last pin is unpinned

--- cores/pin/src/lib.rs
+++ cores/pin/src/lib.rs
@@ -8,14 +8,23 @@ use std::collections::BTreeSet;
  */
 #[derive(Debug, Default, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Pin {
+	/**
+	 * A DAG map containing all pinned content ids. Map is keyed by the referenced content ids. The value is a set
+	 * of tags making pinning and unpinning idempotent. A service may use tags with unique identifiers to ensure
+	 * that a cid stays pinned as it then cannot be unpinned by other services. To unpin, all tags must match.
+	 */
 	pub pins: DagMap<Cid, DagSet<Tags>>,
 }
 
 impl Pin {
+	/**
+	 * A simple function to check if a specific content id is pinned or not
+	 */
 	pub fn is_pinned(&self, cid: &Cid, s: &dyn Storage) -> bool {
 		let pin_map = self.pins.from_link(s);
-		if let Some(_) = pin_map.get(cid) {
-			return true;
+		if let Some(set) = pin_map.get(cid) {
+			// returns true if the tag set for the cid is not empty
+			return !set.from_link(s).is_empty();
 		}
 		false
 	}
@@ -42,27 +51,31 @@ impl Reducer for Pin {
 					// update map
 					pin_map.insert(*cid, DagSet::to_link(current_tags, s));
 				} else {
-					// no tags found -> just insert as new vector
+					// no tags found -> create new set instead
 					let mut new_set = BTreeSet::new();
 					new_set.insert(tags.clone());
+					// update map
 					pin_map.insert(*cid, DagSet::to_link(new_set, s));
 				},
 			PinAction::Unpin(cid, tags) =>
 			// get current tags for cid
 				if let Some(current_tags) = pin_map.get(cid).cloned() {
 					// remove given tag from array
-					let resolved_tags = current_tags.iter(s).filter_map(|t| t.ok()).filter(|t| *t != *tags).collect();
-					let new_tags_tags = DagSet::to_link(resolved_tags, s);
-					// TODO: validate if something got removed?
-
-					// update map
-					pin_map.insert(*cid, new_tags_tags);
+					let filtered_tags: BTreeSet<Tags> =
+						current_tags.iter(s).filter_map(|t| t.ok()).filter(|t| *t != *tags).collect();
+					if filtered_tags.is_empty() {
+						// last tags removed from set -> remove cid from map
+						pin_map.remove(cid);
+					} else {
+						// update map with filtered tags
+						let dag_tags = DagSet::to_link(filtered_tags, s);
+						pin_map.insert(*cid, dag_tags);
+					}
 				} else {
-					// Not currently pinned, cannot unpin!
-					// NOTE: maybe we should return an error here?
+					// Not currently pinned, cannot unpin
 				},
 		};
-		// TODO: update instead of rewrite
+		// TODO: update instead of rewrite?
 		Self { pins: DagMap::to_link(pin_map, s) }
 	}
 }
