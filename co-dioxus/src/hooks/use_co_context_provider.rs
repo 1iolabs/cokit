3d1db2c0e11b44fc4936b4c941a214d2eb3e2dc7
DK <34+dk@users.noreply.1io.com>
2024-04-09 14:42:12 +0200

    feat(co-dioxus): init settings
    Refs: #34

--- co-dioxus/src/hooks/use_co_context_provider.rs
+++ co-dioxus/src/hooks/use_co_context_provider.rs
@@ -1,6 +1,6 @@
-use crate::CoContext;
+use crate::{CoContext, CoSettings};
 use dioxus::prelude::*;
 
-pub fn use_co_context_provider() {
-	use_context_provider(|| CoContext::new());
+pub fn use_co_context_provider(settings: impl FnOnce() -> CoSettings) {
+	use_context_provider(|| CoContext::new(settings()));
 }
