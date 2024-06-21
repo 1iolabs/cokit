809a4c77e543dabbb608d60b1bdf3d14aa9d9f8f
DK <34+dk@users.noreply.1io.com>
2024-06-21 17:12:45 +0200

    ci: build (#19)

--- ci/build.sh
+++ ci/build.sh
@@ -5,9 +5,14 @@ BASE_PATH="$(dirname "$0")/.."
 DOCKERFILE_HASH="$(sha256sum Dockerfile | cut -d' ' -f1)"
 DOCKERFILE_IMAGE="docker-registry.intranet.1io.com/build/co:$DOCKERFILE_HASH"
 
-docker build -f Dockerfile -t "$DOCKERFILE_IMAGE" .
-docker push "$DOCKERFILE_IMAGE"
+docker buildx build --push --platform linux/arm64,linux/amd64 -f Dockerfile -t "$DOCKERFILE_IMAGE" .
 
 echo "variables:" > "$BASE_PATH/ci/build.yml"
 echo "  DOCKERFILE_HASH: $DOCKERFILE_HASH" >> "$BASE_PATH/ci/build.yml"
 echo "  DOCKERFILE_IMAGE: $DOCKERFILE_IMAGE" >> "$BASE_PATH/ci/build.yml"
+
+## Setup buildx:
+# brew install colima
+# docker buildx create --name multiplatform-builder
+# docker buildx use multiplatform-builder
+# docker buildx inspect --bootstrap
