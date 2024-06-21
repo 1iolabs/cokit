b7e506c2cc17afe3b84fede54c317835a71e00f4
DK <34+dk@users.noreply.1io.com>
2024-06-21 11:57:56 +0200

    ci: use prebuild image (#19)

---
+++ ci/README.md
@@ -0,0 +1,4 @@
+# CI Build
+
+This creates an image tagged with the sha256 of the Dockerfile which can then used for the build.
+So everytime the Dockerfile gets changed we need to run the build.sh.
---
+++ ci/build.sh
@@ -0,0 +1,11 @@
+
+BASE_PATH="$(dirname "$0")/.."
+DOCKERFILE_HASH="$(sha256sum Dockerfile | cut -d' ' -f1)"
+DOCKERFILE_IMAGE="docker-registry.intranet.1io.com/build/co:$DOCKERFILE_HASH"
+
+docker build -f Dockerfile -t "$DOCKERFILE_IMAGE" .
+docker push "$DOCKERFILE_IMAGE"
+
+echo "variables:" > "$BASE_PATH/ci/build.yml"
+echo "  DOCKERFILE_HASH: $DOCKERFILE_HASH" >> "$BASE_PATH/ci/build.yml"
+echo "  DOCKERFILE_IMAGE: $DOCKERFILE_IMAGE" >> "$BASE_PATH/ci/build.yml"
