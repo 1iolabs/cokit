f31b09221fc02b79d08eccf6bb8b7bf6f44f38bf
DH <50+dh@users.noreply.1io.com>
2024-07-04 16:47:39 +0200

    feat(co-tauri): prototype for calling commands and subscribing for events

--- co-tauri/src/lib.rs
+++ co-tauri/src/lib.rs
@@ -1,9 +1,12 @@
 use anyhow::anyhow;
-use co_core_membership::Memberships;
-use co_sdk::{Application, ApplicationBuilder, CoId, CoReducerError};
+use co_api::Cid;
+use co_sdk::{Application, ApplicationBuilder, CoId};
+use libipld::{cbor::DagCborCodec, codec::Codec, Ipld};
 use library::co_settings::CoSettings;
-use tauri::{ipc::InvokeError, Wry};
-
+use serde::Deserialize;
+use std::collections::{BTreeMap, BTreeSet};
+use tauri::{ipc::InvokeError, Manager, Wry};
+use tokio_stream::{wrappers::WatchStream, StreamExt};
 pub mod library;
 
 async fn application(settings: CoSettings) -> Application {
@@ -46,36 +49,114 @@ impl From<anyhow::Error> for CoTauriError {
 	}
 }
 
-impl From<CoReducerError> for CoTauriError {
-	fn from(value: CoReducerError) -> Self {
-		Self { error: anyhow::Error::from(value) }
-	}
-}
-
 #[tauri::command]
-async fn tmp_test_command(application: tauri::State<'_, Application>, name: String) -> Result<String, CoTauriError> {
-	let local_reducer = application.local_co_reducer().await?;
-	let membership_state = local_reducer.state::<Memberships>("membership").await?;
-	Ok(format!(
-		"Hello, {}! You've been greeted from Rust! Memberships state for testing: {:#?}",
-		name, membership_state
-	))
+fn tmp_test_command(application: tauri::State<'_, Application>, name: String) -> String {
+	let identifier = &application.settings().identifier;
+	format!("Hello, {}! You've been greeted from Rust! App id: {:#?}", name, identifier)
 }
 
 #[tauri::command]
 async fn get_core_state(
 	application: tauri::State<'_, Application>,
 	co: CoId,
-	core: String,
-) -> Result<Memberships, CoTauriError> {
-	// TODO this currently only works with the membership core. Working on a solution to dynamically get correct core
-	// state.
+	_core: String,
+) -> Result<(Option<Cid>, BTreeSet<Cid>), CoTauriError> {
 	let reducer = application
 		.co_reducer(co.clone())
 		.await?
 		.ok_or(anyhow!("Co not found: {}", co.clone()))?;
-	let state = reducer.state(&core).await?;
-	Ok(state)
+	let (state, heads) = reducer.reducer_state().await;
+	// TODO resolve state up to declared core
+	Ok((state, heads))
+}
+
+#[derive(Deserialize, Debug)]
+struct PushCommandBody {
+	co: CoId,
+	core: String,
+	action: Ipld,
+}
+
+impl TryFrom<Ipld> for PushCommandBody {
+	type Error = anyhow::Error;
+	fn try_from(value: Ipld) -> Result<Self, Self::Error> {
+		match value {
+			Ipld::Map(map) => {
+				let action = PushCommandBody::resolve_action(&map)?;
+				let co = PushCommandBody::resolve_co_id(&map)?;
+				let core = PushCommandBody::resolve_core(&map)?;
+				Ok(PushCommandBody { action, co, core })
+			},
+			_ => Err(anyhow!("Ipld is not a map")),
+		}
+	}
+}
+
+impl PushCommandBody {
+	fn resolve_action(map: &BTreeMap<String, Ipld>) -> Result<Ipld, anyhow::Error> {
+		if let Some(action) = map.get("action") {
+			Ok(action.clone())
+		} else {
+			Err(anyhow!("Body contains no action"))
+		}
+	}
+	fn resolve_co_id(map: &BTreeMap<String, Ipld>) -> Result<CoId, anyhow::Error> {
+		if let Some(ipld) = map.get("co") {
+			match ipld {
+				Ipld::String(co) => Ok(CoId::new(&*co)),
+				_ => Err(anyhow!("Co is not a string")),
+			}
+		} else {
+			Err(anyhow!("Body contains no co info"))
+		}
+	}
+	fn resolve_core(map: &BTreeMap<String, Ipld>) -> Result<String, anyhow::Error> {
+		if let Some(ipld) = map.get("core") {
+			match ipld {
+				Ipld::String(core) => Ok(core.clone()),
+				_ => Err(anyhow!("core not a string")),
+			}
+		} else {
+			Err(anyhow!("body contains no core info"))
+		}
+	}
+}
+
+#[tauri::command]
+async fn push(application: tauri::State<'_, Application>, body: Vec<u8>) -> Result<(), CoTauriError> {
+	let body: PushCommandBody = DagCborCodec::default().decode::<Ipld>(&body)?.try_into()?;
+	tracing::info!("tauri command push: {:#?}", body);
+	let reducer = application
+		.co_reducer(body.co.clone())
+		.await?
+		.ok_or(anyhow!("Co not found: {}", body.co))?;
+	let identity = application.local_identity();
+	reducer.push(&identity, &*body.core, &body.action).await?;
+	Ok(())
+}
+
+#[tauri::command]
+async fn subscribe(
+	application: tauri::State<'_, Application>,
+	app: tauri::AppHandle,
+	co: CoId,
+) -> Result<(), CoTauriError> {
+	tracing::info!("tauri command subscribe: {:#?}", co);
+	let co_reducer = application
+		.co_reducer(co.clone())
+		.await?
+		.ok_or(anyhow!("Co not found: {}", co))?;
+
+	let mut watcher = WatchStream::from_changes(co_reducer.watch().await);
+	while let Some(item) = watcher.next().await {
+		match item {
+			Some((cid, heads)) => {
+				app.emit("test", (cid, heads)).unwrap();
+			},
+			None => (),
+		};
+	}
+	Ok(())
 }
 
 pub async fn tauri_builder(co_settings: CoSettings) -> tauri::Builder<Wry> {
@@ -86,5 +167,5 @@ pub async fn tauri_builder(co_settings: CoSettings) -> tauri::Builder<Wry> {
 	tauri::Builder::default()
 		.plugin(tauri_plugin_shell::init())
 		.manage(application)
-		.invoke_handler(tauri::generate_handler![tmp_test_command, get_core_state])
+		.invoke_handler(tauri::generate_handler![tmp_test_command, get_core_state, push, subscribe])
 }
