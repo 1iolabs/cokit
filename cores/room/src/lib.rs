fae8e57e2ff81acf3596ac76a0d691cf7492f376
DH <50+dh@users.noreply.1io.com>
2025-01-14 16:13:39 +0100

    feat: removed schema generation from room and messaging tests

--- cores/room/src/lib.rs
+++ cores/room/src/lib.rs
@@ -49,18 +49,3 @@ impl Reducer for Room {
 pub extern "C" fn state() {
 	co_api::reduce::<Room>()
 }
-
-#[cfg(test)]
-mod test {
-	use crate::Room;
-	use std::{fs::File, io::Write};
-
-	#[test]
-	fn create_schema() {
-		let schema = schemars::schema_for!(Room);
-		let mut file = File::create("test-output/schema.json").expect("new file");
-		file.write_all(serde_json::to_string_pretty(&schema).expect("json").as_bytes())
-			.expect("file written");
-		println!("{}", serde_json::to_string_pretty(&schema).unwrap());
-	}
-}
