fc213ed79d105b6e9f80916b4280e30fd3700a1a
DH <50+dh@users.noreply.1io.com>
2025-01-16 17:35:14 +0100

    feat(tauri-messenger): add top lvl router

--- tauri-plugin-co-sdk/examples/messenger/src/plugin.tsx
+++ tauri-plugin-co-sdk/examples/messenger/src/plugin.tsx
@@ -1,5 +1,6 @@
 import { TagList } from "@1io/kui-application-sdk";
 import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ListView } from "./components/chats-list.js";
 import { chatsListEpic } from "./epics/index.js";
 import { chatsListReducer } from "./reducers/index.js";
@@ -12,7 +13,12 @@ export default function plugin(pluginTags: TagList): ChatsListPlugin {
         epic: chatsListEpic,
         reducer: chatsListReducer,
         render: (renderTags) => {
-            return <ListView />;
+            return (<BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Navigate to="chats" />} />
+                    <Route path="chats" element={<ListView />} />
+                </Routes>
+            </BrowserRouter>);
         },
         tags: [{ key: "type", value: "coapp-chats-list" }, ...pluginTags],
 
