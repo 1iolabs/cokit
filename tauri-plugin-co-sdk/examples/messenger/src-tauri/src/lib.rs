048e3ffca3acde90a0077941ccd4f5ed9b8bfe4c
DH <50+dh@users.noreply.1io.com>
2025-01-30 17:23:13 +0100

    feat(tauri-messenger): tauri window now has overlay title bar

--- tauri-plugin-co-sdk/examples/messenger/src-tauri/src/lib.rs
+++ tauri-plugin-co-sdk/examples/messenger/src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
 use tauri_plugin_co_sdk::library::co_application::CoApplicationSettings;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -6,6 +7,19 @@ pub async fn run() {
 
 	let co_settings = CoApplicationSettings::new("tauri-app").without_keychain();
 	tauri::Builder::default()
+		.setup(|app| {
+			let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+				.title("")
+				.inner_size(1600.0, 1000.0);
+
+			// set transparent title bar only when building for macOS
+			#[cfg(target_os = "macos")]
+			let win_builder = win_builder.title_bar_style(TitleBarStyle::Overlay);
+
+			win_builder.focused(false).build()?;
+
+			Ok(())
+		})
 		.plugin(tauri_plugin_co_sdk::init(co_settings).await)
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
