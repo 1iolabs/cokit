33f3e2d29df3853949764263acb583717bb87763
DK <34+dk@users.noreply.1io.com>
2025-07-02 10:58:58 +0200

    fix: remove unwrap when open file

--- co-storage/src/library/unixfs_add_file.rs
+++ co-storage/src/library/unixfs_add_file.rs
@@ -1,15 +1,18 @@
 use crate::{unixfs_add, BlockStorage, StorageError};
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use cid::Cid;
 use std::path::Path;
 use tokio_util::compat::TokioAsyncReadCompatExt;
 
 /// Store file to storage and return the (root) CID.
-pub async fn unixfs_add_file<'a, S>(storage: &S, file: impl AsRef<Path>) -> Result<Cid, StorageError>
+pub async fn unixfs_add_file<'a, S>(storage: &S, file: impl AsRef<Path>) -> Result<Cid, anyhow::Error>
 where
 	S: BlockStorage + Send + Sync,
 {
-	let mut handle = tokio::fs::File::open(file.as_ref()).await.unwrap().compat();
+	let mut handle = tokio::fs::File::open(file.as_ref())
+		.await
+		.with_context(|| format!("open file: {:?}", file.as_ref()))?
+		.compat();
 	Ok(unixfs_add(storage, &mut handle)
 		.await?
 		.last()
