7edf0abc78e3220c75fc212f951b63884600dddf
DK <34+dk@users.noreply.1io.com>
2025-09-22 16:33:22 +0200

    fix: running wasm on linux
    - downgrade to wasmer-4 - errors like:
    `cranelift-codegen-0.110.2/src/ir/extname.rs:199:58: index out of
    bounds: the len is 0 but the index is 207`

--- co-runtime/src/library/compile.rs
+++ co-runtime/src/library/compile.rs
@@ -2,7 +2,7 @@ use core::str::FromStr;
 use wasmer::{Module, Store};
 use wasmer_compiler::EngineBuilder;
 use wasmer_compiler_llvm::LLVM;
-use wasmer_types::target::{CpuFeature, Target, Triple};
+use wasmer_types::{CpuFeature, Target, Triple};
 
 pub async fn compile_native(wasm_bytes: impl AsRef<[u8]>, arch_triple: &str) -> Result<Vec<u8>, anyhow::Error> {
 	let mut compiler = EngineBuilder::new(LLVM::default());
