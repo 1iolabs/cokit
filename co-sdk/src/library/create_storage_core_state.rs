e461c64db7ffa9e04307538410532b91c861e92d
DK <34+dk@users.noreply.1io.com>
2025-12-11 11:23:26 +0100

    fix: fmt

--- co-sdk/src/library/create_storage_core_state.rs
+++ co-sdk/src/library/create_storage_core_state.rs
@@ -10,13 +10,11 @@ pub async fn create_storage_core_state(
 ) -> Result<Option<Cid>, anyhow::Error> {
 	Ok(co_core_storage::Storage::initial_state(
 		storage,
-		vec![
-			co_core_storage::StorageAction::PinCreate(
-				CoPinningKey::Root.to_string(co),
-				settings.setting_co_default_max_state(),
-				Default::default(),
-			),
-		],
+		vec![co_core_storage::StorageAction::PinCreate(
+			CoPinningKey::Root.to_string(co),
+			settings.setting_co_default_max_state(),
+			Default::default(),
+		)],
 	)
 	.await?
 	.into())
