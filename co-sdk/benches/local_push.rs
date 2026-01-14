916d81d6237801ffe80a323a681c1ed2265edf4a
DK <34+dk@users.noreply.1io.com>
2026-01-14 16:53:59 +0100

    test: added shared co push bench

--- co-sdk/benches/local_push.rs
+++ co-sdk/benches/local_push.rs
@@ -1,26 +1,40 @@
+use cid::Cid;
 use co_core_co::CoAction;
-use co_sdk::{build_core, crate_repository_path, Application, ApplicationBuilder, CO_CORE_NAME_CO};
+use co_runtime::Core;
+use co_sdk::{build_core, crate_repository_path, Application, ApplicationBuilder, BuildCoreArtifact, CO_CORE_NAME_CO};
+use co_storage::MemoryBlockStorage;
 use co_test::test_log_path;
 use criterion::{criterion_group, criterion_main, Criterion};
-use example_counter::CounterAction;
+use example_counter::{Counter, CounterAction};
 use tokio::runtime::Builder;
 
+async fn build_counter(native: bool) -> (Cid, Core, BuildCoreArtifact) {
+	let core_storage = MemoryBlockStorage::default();
+	let repository_path = crate_repository_path(true).unwrap();
+	let core_path = repository_path.join("examples/counter");
+	let counter_artifact = build_core(repository_path, core_path).unwrap();
+	let counter = counter_artifact.store_artifact(&core_storage).await.unwrap();
+	let native_counter = Core::native::<Counter>();
+	(counter, if native { native_counter } else { Core::Wasm(counter) }, counter_artifact)
+}
+
 async fn setup_local_memory() -> Application {
+	// core
+	let (counter, counter_core, counter_artifact) = build_counter(false).await;
+
+	// application
 	let application = ApplicationBuilder::new_memory("test".to_owned())
 		.with_bunyan_logging(Some(test_log_path()))
 		.with_optional_tracing()
 		.without_keychain()
+		.with_core(counter, counter_core)
 		.build()
 		.await
 		.expect("application");
+
+	// co
 	let local_co = application.local_co_reducer().await.unwrap();
-	let repository_path = crate_repository_path(true).unwrap();
-	let core_path = repository_path.join("examples/counter");
-	let counter = build_core(repository_path, core_path)
-		.unwrap()
-		.store_artifact(&local_co.storage())
-		.await
-		.unwrap();
+	counter_artifact.store_artifact(&local_co.storage()).await.unwrap();
 	local_co
 		.push(
 			&application.local_identity(),
@@ -29,6 +43,8 @@ async fn setup_local_memory() -> Application {
 		)
 		.await
 		.unwrap();
+
+	// result
 	application
 }
 
---
+++ co-sdk/benches/shared_push.rs
@@ -0,0 +1,141 @@
+use cid::Cid;
+use co_core_co::CoAction;
+use co_runtime::Core;
+use co_sdk::{
+	build_core, crate_repository_path, Application, ApplicationBuilder, BuildCoreArtifact, CoId, CoReducerFactory,
+	CreateCo, DidKeyIdentity, DidKeyProvider, CO_CORE_NAME_CO, CO_CORE_NAME_KEYSTORE,
+};
+use co_storage::MemoryBlockStorage;
+use co_test::TmpDir;
+use criterion::{criterion_group, criterion_main, Criterion};
+use example_counter::{Counter, CounterAction};
+use tokio::runtime::Builder;
+
+async fn build_counter(native: bool) -> (Cid, Core, BuildCoreArtifact) {
+	let core_storage = MemoryBlockStorage::default();
+	let repository_path = crate_repository_path(true).unwrap();
+	let core_path = repository_path.join("examples/counter");
+	let counter_artifact = build_core(repository_path, core_path).unwrap();
+	let counter = counter_artifact.store_artifact(&core_storage).await.unwrap();
+	let native_counter = Core::native::<Counter>();
+	(counter, if native { native_counter } else { Core::Wasm(counter) }, counter_artifact)
+}
+
+async fn setup_memory(public: bool) -> Application {
+	// core
+	let (counter, counter_core, counter_artifact) = build_counter(true).await;
+
+	let application = ApplicationBuilder::new_memory("test".to_owned())
+		.without_keychain()
+		.with_core(counter, counter_core)
+		.build()
+		.await
+		.expect("application");
+	let local_co = application.local_co_reducer().await.unwrap();
+
+	// identity
+	let identity = DidKeyIdentity::generate(None);
+	let provider = DidKeyProvider::new(local_co, CO_CORE_NAME_KEYSTORE);
+	provider.store(&identity, None).await.unwrap();
+
+	// create co
+	let co = application
+		.create_co(identity.clone(), CreateCo::new("shared", None).with_public(public))
+		.await
+		.unwrap();
+	counter_artifact.store_artifact(&co.storage()).await.unwrap();
+	co.push(
+		&identity,
+		CO_CORE_NAME_CO,
+		&CoAction::CoreCreate { core: "counter".to_owned(), binary: counter, tags: Default::default() },
+	)
+	.await
+	.unwrap();
+
+	// result
+	application
+}
+
+async fn setup_file(public: bool) -> (Application, TmpDir) {
+	let tmp = TmpDir::new("co");
+	let application = ApplicationBuilder::new_with_path("test", tmp.path().to_owned())
+		.without_keychain()
+		.build()
+		.await
+		.expect("application");
+	let local_co = application.local_co_reducer().await.unwrap();
+
+	// identity
+	let identity = DidKeyIdentity::generate(None);
+	let provider = DidKeyProvider::new(local_co, CO_CORE_NAME_KEYSTORE);
+	provider.store(&identity, None).await.unwrap();
+
+	// create co
+	let co = application
+		.create_co(identity.clone(), CreateCo::new("shared", None).with_public(public))
+		.await
+		.unwrap();
+	let repository_path = crate_repository_path(true).unwrap();
+	let core_path = repository_path.join("examples/counter");
+	let counter = build_core(repository_path, core_path)
+		.unwrap()
+		.store_artifact(&co.storage())
+		.await
+		.unwrap();
+	co.push(
+		&identity,
+		CO_CORE_NAME_CO,
+		&CoAction::CoreCreate { core: "counter".to_owned(), binary: counter, tags: Default::default() },
+	)
+	.await
+	.unwrap();
+
+	// result
+	(application, tmp)
+}
+
+async fn shared_push(co_id: CoId, application: Application) {
+	let co = application.context().try_co_reducer(&co_id).await.unwrap();
+	co.push(&application.local_identity(), "counter", &CounterAction::Increment(1))
+		.await
+		.unwrap();
+}
+
+fn benchmark(c: &mut Criterion) {
+	let runtime = Builder::new_multi_thread().enable_all().build().unwrap();
+	let application = runtime.block_on(setup_memory(false));
+	let co_id = CoId::from("shared");
+	c.bench_function("shared_push", move |b| {
+		b.to_async(&runtime).iter(|| shared_push(co_id.clone(), application.clone()))
+	});
+}
+
+fn public_benchmark(c: &mut Criterion) {
+	let runtime = Builder::new_multi_thread().enable_all().build().unwrap();
+	let application = runtime.block_on(setup_memory(true));
+	let co_id = CoId::from("shared");
+	c.bench_function("shared_push (public)", move |b| {
+		b.to_async(&runtime).iter(|| shared_push(co_id.clone(), application.clone()))
+	});
+}
+
+fn file_benchmark(c: &mut Criterion) {
+	let runtime = Builder::new_multi_thread().enable_all().build().unwrap();
+	let (application, _tmp) = runtime.block_on(setup_file(false));
+	let co_id = CoId::from("shared");
+	c.bench_function("shared_push (file)", move |b| {
+		b.to_async(&runtime).iter(|| shared_push(co_id.clone(), application.clone()))
+	});
+}
+
+fn file_public_benchmark(c: &mut Criterion) {
+	let runtime = Builder::new_multi_thread().enable_all().build().unwrap();
+	let (application, _tmp) = runtime.block_on(setup_file(true));
+	let co_id = CoId::from("shared");
+	c.bench_function("shared_push (file; public)", move |b| {
+		b.to_async(&runtime).iter(|| shared_push(co_id.clone(), application.clone()))
+	});
+}
+
+criterion_group!(benches, benchmark, public_benchmark, file_benchmark, file_public_benchmark);
+criterion_main!(benches);
